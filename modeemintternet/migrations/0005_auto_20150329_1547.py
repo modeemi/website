8d2000152f550cfdb3bf3b2af02517007233bbbb
aleksih <aleksi.hakli@gmail.com>
2015-03-29 16:03:47 +0300

    Event model updates

---
+++ modeemintternet/migrations/0005_auto_20150329_1547.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('modeemintternet', '0004_auto_20150315_0131'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='event',
+            name='lat',
+            field=models.FloatField(default=0.0),
+            preserve_default=True,
+        ),
+        migrations.AlterField(
+            model_name='event',
+            name='lon',
+            field=models.FloatField(default=0.0),
+            preserve_default=True,
+        ),
+    ]

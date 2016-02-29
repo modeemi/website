e11a975ade758bc057f308568d6f514c28bf9a8f
Aleksi Häkli <aleksi.hakli@gmail.com>
2016-02-29 11:35:23 +0200

    Update Crispy forms as well

---
+++ modeemintternet/migrations/0010_auto_20160229_1132.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('modeemintternet', '0009_auto_20160229_1131'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='soda',
+            options={'verbose_name': 'Limu', 'verbose_name_plural': 'Limut'},
+        ),
+    ]

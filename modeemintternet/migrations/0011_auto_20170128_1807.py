a784f3bbeabd84055f66a9466aebe3c74cdb00f7
Aleksi Häkli <aleksi.hakli@vincit.fi>
2017-01-28 18:07:53 +0200

    Add missing shell migrations

---
+++ modeemintternet/migrations/0011_auto_20170128_1807.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('modeemintternet', '0010_auto_20160229_1132'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='application',
+            name='shell',
+            field=models.CharField(choices=[('/bin/sh', '/bin/sh'), ('/bin/bash', '/bin/bash'), ('/bin/zsh', '/bin/zsh'), ('/bin/tcsh', '/bin/tcsh'), ('/bin/false', '/bin/false')], default='/bin/bash', max_length=32),
+        ),
+    ]

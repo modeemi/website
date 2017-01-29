bd4d1584b8e9b4c4453238cbd674cdc745f4530d
Aleksi Häkli <aleksi.hakli@vincit.fi>
2017-01-29 23:12:22 +0200

    Fix module name in wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,6 +8,6 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "modeemintternet.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

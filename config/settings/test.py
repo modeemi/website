0db01e939d1ba9ebfbfd678432ba08b32c5f3d8f
Aleksi Häkli <aleksi.hakli@iki.fi>
2018-02-11 13:31:50 +0200

    Disable RECAPTCHA in tests

--- config/settings/test.py
+++ config/settings/test.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from .local import *  # noqa
 
 SECRET_KEY = env('DJANGO_SECRETKEY', default='thisisthedummydjangosecretkey')
 RECAPTCHA_PUBLIC_KEY = env('RECAPTCHA_PUBLIC_KEY', default='thisisthedummyrecaptchapublickey')
 RECAPTCHA_PRIVATE_KEY = env('RECAPTCHA_PRIVATE_KEY', default='thisisthedummyrecaptchaprivatekey')
+
+os.environ['RECAPTCHA_DISABLE'] = 'True'

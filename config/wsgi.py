e874cfbe4629bcb8ef2bcf3af67a9ec9f19466e1
Aleksi Häkli <aleksi.hakli@iki.fi>
2018-02-11 13:35:04 +0200

    Clean up wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,16 +2,18 @@
 
 from __future__ import unicode_literals
 
-"""
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+'''
 WSGI config for modeemintternet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-"""
+'''
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 application = get_wsgi_application()

9db5192d066e1e6d5e23aca617358b25025232e8
Aleksi Häkli <aleksi.hakli@iki.fi>
2019-09-14 20:14:55 +0300

    Clean up views module structure

--- modeemintternet/views.py
+++ modeemintternet/views.py
@@ -1,6 +1,6 @@
-import datetime
-import logging
-import re
+from datetime import datetime
+from logging import getLogger
+from re import split
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required, permission_required
@@ -14,12 +14,133 @@ from modeemintternet.models import News, Soda, Membership, MembershipFee
 from modeemintternet.forms import ApplicationForm, FeedbackForm, MembershipForm, MembershipFeeForm
 from modeemintternet.tasks import activate
 
-logger = logging.getLogger(__name__)
+logger = getLogger(__name__)
+
+
+def sitemap(request):
+    return render(request, 'sitemap.xml', content_type='application/xml')
 
 
 def etusivu(request):
     news = News.objects.order_by('-posted')[:10]
     return render(request, 'etusivu.html', {'news': news})
+
+
+def yhdistys(request):
+    return render(request, 'yhdistys.html')
+
+
+def saannot(request):
+    return render(request, 'saannot.html')
+
+
+def rekisteriseloste(request):
+    return render(request, 'rekisteriseloste.html')
+
+
+def hallitus(request):
+    return render(request, 'hallitus.html')
+
+
+def yhteystiedot(request):
+    return render(request, 'yhteystiedot.html')
+
+
+def backup(request):
+    return render(request, 'backup.html')
+
+
+def password(request):
+    return render(request, 'password.html')
+
+
+def halutaan(request):
+    return render(request, 'halutaan.html')
+
+
+def laitteisto(request):
+    return render(request, 'laitteisto.html')
+
+
+def palvelut(request):
+    sodas = Soda.objects.filter(active=True).order_by('price', 'name')
+    return render(request, 'palvelut.html', {'sodas': sodas})
+
+
+def uutiset(request, pk=None):
+    if pk:
+        return render(request, 'uutiset.html', {
+            'news': News.objects.filter(pk=pk),
+        })
+    return render(request, 'uutiset.html', {
+        'news': News.objects.order_by('-id'),
+    })
+
+
+def palaute(request):
+    if not request.method == 'POST':
+        return render(request, 'palaute.html', {'form': FeedbackForm()})
+
+    feedback_form = FeedbackForm(request.POST)
+    if not feedback_form.is_valid():
+        return render(request, 'palaute.html', {
+            'form': feedback_form,
+        }, status=400)
+
+    feedback = feedback_form.save()
+    mailer.feedback_received(feedback)
+    success = True
+
+    return render(request, 'palaute.html', {
+        'form': feedback_form,
+        'success': success,
+    })
+
+
+def jaseneksi(request):
+    if not request.POST:
+        return render(request, 'jaseneksi.html', {'form': ApplicationForm()})
+
+    # Check form validity for password errors
+    application_form = ApplicationForm(request.POST)
+
+    if not application_form.is_valid():
+        return render(request, 'jaseneksi.html', {
+            'form': application_form,
+        }, status=400)
+
+    # Password is not saved in the form, so we check it manually
+    # and notify of errors in context the data, if there are any.
+    password_matches = \
+        request.POST.get('password') == request.POST.get('password_check')
+
+    # If the form is else valid but the password doesn't match or is empty,
+    # mark the form as invalid and display a custom password error message.
+    if not password_matches:
+        error_msg = 'Salasana ja tarkiste eivät täsmää.'
+        application_form.add_error('password', '')
+        application_form.add_error('password_check', error_msg)
+
+        return render(request, 'jaseneksi.html', {
+            'form': application_form,
+        }, status=400)
+
+    application = application_form.save()
+    application.generate_password_hashes(request.POST.get('password'))
+
+    # Try and send mail about the application, otherwise log and show error message.
+    try:
+        mailing_success = True
+        mailer.application_created(application)
+    except Exception as e:
+        mailing_success = False
+        logger.error('Failed to send mail about the new application: %e', e)
+
+    # Return info page for the application.
+    return render(request, 'jaseneksi.html', {
+        'success': True,
+        'mailing_success': mailing_success,
+    })
 
 
 @login_required
@@ -112,7 +233,7 @@ def kayttajarekisteri_jasenmaksut(request):
             membership_fee, _ = MembershipFee.objects.get_or_create(year=year)
 
             usernames = list(filter(None, map(str.lower, map(str.strip,
-                re.split(r'[\s,]+', form.cleaned_data['usernames']))
+                split(r'[\s,]+', form.cleaned_data['usernames']))
             )))
 
             memberships = Membership.objects.filter(user__username__in=usernames)
@@ -125,124 +246,3 @@ def kayttajarekisteri_jasenmaksut(request):
             return HttpResponseRedirect(reverse('kayttajarekisteri'))
 
     return render(request, 'rekisteri/jasenmaksut.html')
-
-
-def yhdistys(request):
-    return render(request, 'yhdistys.html')
-
-
-def palvelut(request):
-    sodas = Soda.objects.filter(active=True).order_by('price', 'name')
-    return render(request, 'palvelut.html', {'sodas': sodas})
-
-
-def laitteisto(request):
-    return render(request, 'laitteisto.html')
-
-
-def palaute(request):
-    if not request.method == 'POST':
-        return render(request, 'palaute.html', {'form': FeedbackForm()})
-
-    feedback_form = FeedbackForm(request.POST)
-    if not feedback_form.is_valid():
-        return render(request, 'palaute.html', {
-            'form': feedback_form,
-        }, status=400)
-
-    feedback = feedback_form.save()
-    mailer.feedback_received(feedback)
-    success = True
-
-    return render(request, 'palaute.html', {
-        'form': feedback_form,
-        'success': success,
-    })
-
-
-def saannot(request):
-    return render(request, 'saannot.html')
-
-
-def rekisteriseloste(request):
-    return render(request, 'rekisteriseloste.html')
-
-
-def hallitus(request):
-    return render(request, 'hallitus.html')
-
-
-def yhteystiedot(request):
-    return render(request, 'yhteystiedot.html')
-
-
-def backup(request):
-    return render(request, 'backup.html')
-
-
-def password(request):
-    return render(request, 'password.html')
-
-
-def halutaan(request):
-    return render(request, 'halutaan.html')
-
-
-def jaseneksi(request):
-    if not request.POST:
-        return render(request, 'jaseneksi.html', {'form': ApplicationForm()})
-
-    # Check form validity for password errors
-    application_form = ApplicationForm(request.POST)
-
-    if not application_form.is_valid():
-        return render(request, 'jaseneksi.html', {
-            'form': application_form,
-        }, status=400)
-
-    # Password is not saved in the form, so we check it manually
-    # and notify of errors in context the data, if there are any.
-    password_matches = \
-        request.POST.get('password') == request.POST.get('password_check')
-
-    # If the form is else valid but the password doesn't match or is empty,
-    # mark the form as invalid and display a custom password error message.
-    if not password_matches:
-        error_msg = 'Salasana ja tarkiste eivät täsmää.'
-        application_form.add_error('password', '')
-        application_form.add_error('password_check', error_msg)
-
-        return render(request, 'jaseneksi.html', {
-            'form': application_form,
-        }, status=400)
-
-    application = application_form.save()
-    application.generate_password_hashes(request.POST.get('password'))
-
-    # Try and send mail about the application, otherwise log and show error message.
-    try:
-        mailing_success = True
-        mailer.application_created(application)
-    except Exception as e:
-        mailing_success = False
-        logger.error('Failed to send mail about the new application: %e', e)
-
-    # Return info page for the application.
-    return render(request, 'jaseneksi.html', {
-        'success': True,
-        'mailing_success': mailing_success,
-    })
-
-
-def uutiset(request, pk=None):
-    if pk:
-        return render(request, 'uutiset.html', {
-            'news': News.objects.filter(pk=pk),
-        })
-    return render(request, 'uutiset.html', {
-        'news': News.objects.order_by('-id'),
-    })
-
-
-def sitemap(request):
-    return render(request, 'sitemap.xml', content_type='application/xml')

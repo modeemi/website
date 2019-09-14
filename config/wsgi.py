01d003af9564a00b4a9237dfa6b5f0c146b35c9e
Aleksi Häkli <aleksi.hakli@iki.fi>
2019-09-14 20:22:48 +0300

    Use double quotes on docstrings

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,11 +1,11 @@
-'''
+"""
 WSGI config for modeemintternet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
-'''
+"""
 
 import os
 

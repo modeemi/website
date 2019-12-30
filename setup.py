ec41b6b0803de370b128ef65ebcf7c14b4378be1
Aleksi Häkli <aleksi.hakli@iki.fi>
2019-12-30 16:26:11 +0200

    Upgrade to Python 3.8

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Communications",
     ],
     zip_safe=False,

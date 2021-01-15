ea8508751059d5bbc57bb1e73e5dc54dd71af67e
Aleksi Häkli <aleksi.hakli@iki.fi>
2021-01-15 18:16:52 +0200

    Test speed optimization settings

--- config/settings/test.py
+++ config/settings/test.py
@@ -7,3 +7,13 @@ RECAPTCHA_PUBLIC_KEY = env("RECAPTCHA_PUBLIC_KEY", default=None)
 RECAPTCHA_PRIVATE_KEY = env("RECAPTCHA_PRIVATE_KEY", default=None)
 
 os.environ.setdefault("RECAPTCHA_DISABLE", "True")
+
+# http://www.eatsomecode.com/faster-django-tests
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+DEBUG_LOGGING = False
+THUMBNAIL_DEBUG = False  # sorl
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+DEFAULT_FILE_STORAGE = "inmemorystorage.InMemoryStorage"

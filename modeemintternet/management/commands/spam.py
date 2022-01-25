d475f5bc60b79f6b5bc33dab1f50bd415157fa75
Aleksi Häkli <aleksi.hakli@iki.fi>
2022-01-25 16:43:15 +0200

    Fix fuxi koodi

--- modeemintternet/management/commands/spam.py
+++ modeemintternet/management/commands/spam.py
@@ -1,11 +1,20 @@
+from logging import getLogger
+
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
+from django.db import transaction
+from django.db.models import Q
 
-from modeemintternet.tasks import remind
 from modeemintternet.models import Membership
 
+logger = getLogger(__name__)
+
+ORGANIZATION_EMAIL = settings.DEFAULT_FROM_EMAIL
+
+
 @transaction.atomic
-def spam(subject, message, memberships=None) -> List[str]:
+def spam(subject, message, memberships=None):
     """
     Send message to all members.
     """
@@ -14,26 +23,23 @@ def spam(subject, message, memberships=None) -> List[str]:
     if memberships is None:
         memberships = Membership.objects.exclude(Q(user__is_active=False))
 
-    mailed = list(memberships.values_list("user__username", flat=True))
-
     if settings.MODE_DRY_RUN:
-        logger.info("Would send to: %s", recipients)
+        reminded = list(memberships.values_list("user__username", flat=True))
+        logger.info("Would send to: %s", reminded)
         logger.info("Subject: %s", subject)
         logger.info("Body:")
-        logger.info("%s", body)
-        return recipients
+        logger.info(message)
+        return reminded
 
+    reminded = []
     for membership in memberships:
         try:
             send_mail(subject, message, ORGANIZATION_EMAIL, [membership.user.email])
+            reminded.append(membership.user.username)
         except Exception as e:
-            logger.exception(
-                    "Sending email to %s failed",
-                    membership.user,
-                    exc_info=e,
-                    )
-
+            logger.exception("Sending email to %s failed", membership.user, exc_info=e)
     return reminded
+
 
 class Command(BaseCommand):
     help = "Remind user accounts that have unpaid membership fees"
@@ -48,11 +54,11 @@ class Command(BaseCommand):
                 line = input()
             except EOFError:
                 break
-            contents.append(line)
-        body = '\n'.join(lines)
+            lines.append(line)
+        message = "\n".join(lines)
         print("Press enter to confirm, ctrl-C to cancel.")
         input()
-        spammed = spam()
+        spammed = spam(subject, message)
         self.stdout.write(
-            self.style.SUCCESS(f"Successfully reminded {len(spammed)} accounts")
-            )
+            self.style.SUCCESS(f"Successfully spammed {len(spammed)} accounts")
+        )
